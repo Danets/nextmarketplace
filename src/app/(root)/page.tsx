86811c1d0391ad2a68fa675180c367d817441e14
Valeriy Danets <dantesvv@gmail.com>
2025-05-16 10:56:41 +0300

    Has been added @clerk/nextjs

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,16 @@
+import {
+  SignedIn,
+  UserButton,
+} from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <>
       <h1>Hello Admin!</h1>
       <p>Welcome to the admin dashboard.</p>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </>
 
   );
