131f461c9a8af2b386715f19a77e2e26f15c5aef
Valeriy Danets <dantesvv@gmail.com>
2025-10-31 14:54:27 +0200

    Adding DataTable component

--- src/app/(dashboard)/[storeId]/(routes)/billboards/_components/billboard-client.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/_components/billboard-client.tsx
@@ -5,10 +5,11 @@ import { Heading } from "@/components/heading"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { Plus } from "lucide-react"
-import { Billboard } from "@prisma/client";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "./data-table";
 
 interface BillboardClientProps {
-    billboards: Billboard[];
+    billboards: BillboardColumn[];
 }
 
 export const BillboardClient = ({ billboards }: BillboardClientProps) => {
@@ -27,20 +28,10 @@ export const BillboardClient = ({ billboards }: BillboardClientProps) => {
                     Add new
                 </Button>
             </div>
-            <ul>
-                {billboards.length > 0 ?
-                    (billboards.map((billboard: Billboard) => (
-                        <li
-                            key={billboard.id}
-                            className="text-sm text-gray-600 hover:cursor-pointer hover:bg-indigo-700"
-                            onClick={() => router.push(`/${storeId}/billboards/${billboard.id}`)}
-                        >
-                            {billboard.label}
-                        </li>
-                    ))) : "Loading..."
-                }
-            </ul>
             <Separator />
+            <div className="container mx-auto py-10">
+                <DataTable columns={columns} data={billboards} storeId={storeId} />
+            </div>
         </>
     )
 }
--- src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,8 +1,11 @@
 import { prisma } from "@/lib/db";
-import { BillboardClient } from "./_components/billboard-client";
+import { format } from "date-fns";
 
-export default async function BillboardsPage({ params }: { params: { storeId: string } }) {
-    const { storeId } = params;
+import { BillboardClient } from "./_components/billboard-client";
+import { BillboardColumn } from "./_components/columns";
+
+export default async function BillboardsPage({ params }: { params: Promise<{ storeId: string }> }) {
+    const { storeId } = await params;
 
     const billboards = await prisma.billboard.findMany({
         where: { storeId },
@@ -11,10 +14,17 @@ export default async function BillboardsPage({ params }: { params: { storeId: st
         }
     });
 
+    const formattedBillboards: BillboardColumn[] = billboards.map((billboard) => ({
+        id: billboard.id,
+        label: billboard.label,
+        createdAt: format(billboard.createdAt, "MMMM do, yyyy")
+    })
+    );
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient billboards={billboards} />
+                <BillboardClient billboards={formattedBillboards} />
             </div>
         </div>
     )
